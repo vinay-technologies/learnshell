f53c7192a0fb7c777c061c9b57aabe7573d150eb
vinaymeruva <vinaymeruva@gmail.com>
2023-02-03 11:18:33 +0530

    new file added

--- 01-example.sh
+++ 01-example.sh
@@ -1,7 +1,10 @@
-welcome to roboshop shell project
-welcome to the project.
+#!/bin/bash
 
 
+#welcome to roboshop shell project
+#welcome to the project.
+
+# cat user file
 cat /etc/passwd | head -2
-
+# grep root user from user file
 grep root /etc/poasswd
